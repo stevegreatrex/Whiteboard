37434ca26ddff0be16e97d577eecde54f20fd9d6
Steve Greatrex <stephen.greatrex@googlemail.com>
2012-08-02 19:44:56 +0100

    Created project & added references

---
+++ Whiteboard/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Whiteboard
+{
+	public class FilterConfig
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+	}
+}
